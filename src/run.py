b32bb9ad09efe59a6b8935b1aae84f385f29377d
Pontus Frendin <pontus@frend.in>
2024-02-03 18:53:24 +0000

    Fiddle with message

--- src/run.py
+++ src/run.py
@@ -3,8 +3,11 @@ import ollama
 stream = ollama.chat(
     model="llama2",
     messages=[
-        {"role": "system", "content": "Use max three sentences when asked a question."},
-        {"role": "user", "content": "Why is the sky blue?"},
+        {
+            "role": "system",
+            "content": "Use max three sentences in your answer when asked a question.",
+        },
+        {"role": "user", "content": "Why is the sky pink sometimes at sunset?"},
     ],
     stream=True,
 )
